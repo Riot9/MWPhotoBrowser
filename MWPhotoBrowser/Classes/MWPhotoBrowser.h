6ec7c484c80cfc1fa14cb9358d0c880b28020354
Erik Hope <erikjhope@gmail.com>
2012-03-08 15:02:54 -0600

    Makes everything in MWPhotoBrowser available to subclass

--- MWPhotoBrowser/Classes/MWPhotoBrowser.h
+++ MWPhotoBrowser/Classes/MWPhotoBrowser.h
@@ -20,7 +20,7 @@
 #endif
 
 // Delgate
-@class MWPhotoBrowser, MWZoomingScrollView;
+@class MWPhotoBrowser, MWZoomingScrollView, MBProgressHUD;
 @protocol MWPhotoBrowserDelegate <NSObject>
 - (NSUInteger)numberOfPhotosInPhotoBrowser:(MWPhotoBrowser *)photoBrowser;
 - (id<MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index;
@@ -30,6 +30,45 @@
 
 // MWPhotoBrowser
 @interface MWPhotoBrowser : UIViewController <UIScrollViewDelegate, UIActionSheetDelegate, MFMailComposeViewControllerDelegate> 
+{
+    
+	// Data
+    id <MWPhotoBrowserDelegate> _delegate;
+    NSUInteger _photoCount;
+    NSMutableArray *_photos;
+	NSArray *_depreciatedPhotoData; // Depreciated
+	
+	// Views
+	UIScrollView *_pagingScrollView;
+	
+	// Paging
+	NSMutableSet *_visiblePages, *_recycledPages;
+	NSUInteger _currentPageIndex;
+	NSUInteger _pageIndexBeforeRotation;
+	
+	// Navigation & controls
+	UIToolbar *_toolbar;
+	NSTimer *_controlVisibilityTimer;
+	UIBarButtonItem *_previousButton, *_nextButton, *_actionButton;
+    UIActionSheet *_actionsSheet;
+    MBProgressHUD *_progressHUD;
+    
+    // Appearance
+    UIImage *_navigationBarBackgroundImageDefault, 
+    *_navigationBarBackgroundImageLandscapePhone;
+    UIColor *_previousNavBarTintColor;
+    UIBarStyle _previousNavBarStyle;
+    UIStatusBarStyle _previousStatusBarStyle;
+    UIBarButtonItem *_previousViewControllerBackButton;
+    
+    // Misc
+    BOOL _displayActionButton;
+	BOOL _performingLayout;
+	BOOL _rotating;
+    BOOL _viewIsActive; // active as in it's in the view heirarchy
+    BOOL _didSavePreviousStateOfNavBar;
+}
+
 
 // Properties
 @property (nonatomic) BOOL displayActionButton;
@@ -57,6 +96,59 @@
 - (void)cancelControlHiding;
 
 - (void)updateNavigation;
+
+
+// Private Properties
+@property (nonatomic, retain) UIColor *previousNavBarTintColor;
+@property (nonatomic, retain) UIBarButtonItem *previousViewControllerBackButton;
+@property (nonatomic, retain) UIImage *navigationBarBackgroundImageDefault, *navigationBarBackgroundImageLandscapePhone;
+@property (nonatomic, retain) UIActionSheet *actionsSheet;
+@property (nonatomic, retain) MBProgressHUD *progressHUD;
+
+// Private Methods
+
+// Layout
+- (void)performLayout;
+
+// Nav Bar Appearance
+- (void)setNavBarAppearance:(BOOL)animated;
+- (void)storePreviousNavBarAppearance;
+- (void)restorePreviousNavBarAppearance:(BOOL)animated;
+
+// Paging
+- (void)tilePages;
+- (BOOL)isDisplayingPageForIndex:(NSUInteger)index;
+- (MWZoomingScrollView *)pageDisplayingPhoto:(id<MWPhoto>)photo;
+- (MWZoomingScrollView *)dequeueRecycledPage;
+- (void)configurePage:(MWZoomingScrollView *)page forIndex:(NSUInteger)index;
+
+// Frames
+- (CGRect)frameForPagingScrollView;
+- (CGRect)frameForPageAtIndex:(NSUInteger)index;
+- (CGSize)contentSizeForPagingScrollView;
+- (CGPoint)contentOffsetForPageAtIndex:(NSUInteger)index;
+- (CGRect)frameForToolbarAtOrientation:(UIInterfaceOrientation)orientation;
+- (CGRect)frameForCaptionView:(MWCaptionView *)captionView atIndex:(NSUInteger)index;
+
+// Navigation
+- (void)gotoPreviousPage;
+- (void)gotoNextPage;
+
+// Controls
+- (void)hideControlsAfterDelay;
+- (void)toggleControls;
+- (BOOL)areControlsHidden;
+
+// Data
+- (id<MWPhoto>)photoAtIndex:(NSUInteger)index;
+- (UIImage *)imageForPhoto:(id<MWPhoto>)photo;
+- (void)loadAdjacentPhotosIfNecessary:(id<MWPhoto>)photo;
+
+// Actions
+- (void)savePhoto;
+- (void)copyPhoto;
+- (void)emailPhoto;
+
 
 
 @end
